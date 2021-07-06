28d58e4c2e2da25d6e3bd8ac32dfb4d2886390b5
Megan Leet <megan.leet@iinet.net.au>
2021-07-06 20:43:08 +1000

    Add a build script

---
+++ build.sh
@@ -0,0 +1,18 @@
+mkdir build
+
+pushd BasiliskII/src/Unix
+NO_CONFIGURE=1 ./autogen.sh
+#might want --with-libvhd or --with-vdeplug? Just need to make sure that doesn't break anything
+./configure --enable-standalone-gui --with-bincue --enable-addressing=banks
+make -j`nproc`
+cp BasiliskII BasiliskIIGUI ../../../build
+popd
+
+pushd SheepShaver/src/Unix
+NO_CONFIGURE=1 ./autogen.sh
+#--with-bincue broke last time I tried it, should do that again
+./configure --enable-standalone-gui --enable-addressing=direct
+make -j`nproc`
+cp SheepShaver SheepShaverGUI ../../../build
+
+popd
