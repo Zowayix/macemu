193cfa20c33e86316a47611e9417699e4f74ca8a
Megan Leet <megan.leet@iinet.net.au>
2021-07-06 21:17:29 +1000

    whoops

--- build.sh
+++ build.sh
@@ -1,9 +1,10 @@
-mkdir build
+mkdir -p build
 
 pushd BasiliskII/src/Unix
 NO_CONFIGURE=1 ./autogen.sh
 #might want --with-libvhd or --with-vdeplug? Just need to make sure that doesn't break anything
-./configure --enable-standalone-gui --with-bincue --enable-addressing=banks
+#JIT requires addressing=direct, do I care about that…
+./configure --enable-standalone-gui --with-bincue --enable-addressing=banks --disable-jit
 make -j`nproc`
 cp BasiliskII BasiliskIIGUI ../../../build
 popd
